56d56f9e5d44ab4710295f52b0bd648d226c4347
Walyson Menezes <menezeswalyson@gmail.com>
2025-10-21 11:37:10 -0300

    Subindo exercicios das funcoes

---
+++ exercicio4/Program.cs
@@ -0,0 +1,14 @@
+﻿int senha = 1234;
+int senhaDigitada = 0;
+
+Console.WriteLine("Digite a senha correta de 4 digitos");
+senhaDigitada = int.Parse(Console.ReadLine());
+
+if (senha == senhaDigitada)
+{
+    Console.WriteLine("Senha Correta");
+}
+else
+{
+    Console.WriteLine("Senha Incorreta");
+}
---
+++ exercicio5/Program.cs
@@ -0,0 +1,21 @@
+﻿int quantidade;
+double preco = 0;
+double total;
+
+Console.Clear();
+Console.Write("Digite o número de maçãs compradas: ");
+quantidade = int.Parse(Console.ReadLine());
+
+total = preco * quantidade;
+
+if (quantidade >= 12)
+{
+    preco = 0.25;
+}
+else
+{
+    preco = 0.30;
+}
+
+
+Console.WriteLine($"O valor total da compra é: R$ {preco * quantidade}");
